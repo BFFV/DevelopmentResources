6a95b6b49f85c7fe594376778be6f36be2506d79
BFFV <bffarias@uc.cl>
2020-09-09 03:34:22 -0300

    :sparkles: :ok_hand: Tested sentiment analysis library.

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from classifier import SentimentClassifier
 
 
 class Text(models.Model):
@@ -6,6 +7,10 @@ class Text(models.Model):
     content = models.TextField()
 
     objects = models.Manager()
+
+    def get_sentiment(self):
+        clf = SentimentClassifier()
+        return clf.predict(self.content)
 
     def __str__(self):
         return self.content
--- backend/api/views.py
+++ backend/api/views.py
@@ -6,5 +6,5 @@ from .models import Text
 class TextViewSet(viewsets.ModelViewSet):
     queryset = Text.objects.all().order_by('author')
     for text in queryset:
-        print(text)
+        print(f'Text: {text.content}, Prediction: {text.get_sentiment()}')
     serializer_class = TextSerializer
